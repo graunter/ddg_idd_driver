7db33666dcc2671fd376855ec15e96a8b1d8f3a3
ZVS <graunter@gmail.com>
2024-05-14 16:54:39 +0600

    Fix for review

--- project/scripts/package/etc/ddg-idd-driver/start.sh
+++ project/scripts/package/etc/ddg-idd-driver/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #/usr/bin/python3 /etc/ddg-idd-driver/main.py -d info &>> /etc/ddg-idd-driver/ddg-idd-driver.log & #> /dev/null 2>&1
-./opt/ddg-idd-driver/ddg-idd-driver -d info &>> /etc/ddg-idd-driver/ddg-idd-driver.log & #> /dev/null 2>&1
+./opt/ddg-idd-driver/ddg-idd-driver -d info &>> /var/log/ddg-idd-driver/ddg-idd-driver.log & #> /dev/null 2>&1
 
 echo $! > /var/run/ddg-idd-driver.pid
